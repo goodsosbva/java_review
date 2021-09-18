bef3181d73c7f0f446398e16f99793fd7934895a
goodsosbva <62534722+goodsosbva@users.noreply.github.com>
2021-09-18 19:01:54 +0900

    Add files via upload

---
+++ hellojava.java
@@ -0,0 +1,18 @@
+package first;
+
+public class hellojava {
+
+	public static void main(String[] args) {
+		System.out.println("long time no see, java!");
+		int i;
+		double j;
+		
+		i = 10;
+		j = 2.0;
+		double res;
+		res = i + j;
+		System.out.println(res);
+
+	}
+
+}
