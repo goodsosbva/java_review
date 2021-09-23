a6aff7dbbc613dd9f5b3c9e422255f6974793935
goodsosbva <62534722+goodsosbva@users.noreply.github.com>
2021-09-23 17:51:06 +0900

    Add files via upload

---
+++ InheritancePractice/CustomerTest1.java
@@ -0,0 +1,14 @@
+package first.InheritancePractice;
+
+public class CustomerTest1 {
+
+	public static void main(String[] args) {
+		  VIPCustomer customerKim = new VIPCustomer();  // 하위 클래스 생성
+		  customerKim.setCustomerID(10020);
+		  customerKim.setCustomerName("김유신");
+		  customerKim.bonusPoint = 10000;
+		  System.out.println(customerKim.showCustomerInfo());
+
+	}
+
+}
